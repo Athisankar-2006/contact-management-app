7a033cd6d3fb9771dd21653adbf533562e5ccec4
Athisankar-2006 <athisankar2k6@gmail.com>
2026-04-12 15:56:31 +0530

    server is runing

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("haii i am running");
+
+const express=require("express");
+const app=express();
+
+app.get("/api/",(req,res)=>{
+    res.send("server is running now");
+})
+
+const PORT=4000;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
+});
