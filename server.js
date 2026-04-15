c62d6915ff4369c65fe3fc4ad70abacad85bf8dc
Athisankar-2006 <athisankar2k6@gmail.com>
2026-04-15 23:23:40 +0530

    error handler and middleware added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express=require("express");
 const app=express();
+const errorHandler=require("./middleware/errorhandler")
 const dotenv=require("dotenv").config();
 
 const port= process.env.PORT ||4000;
@@ -8,6 +9,7 @@ const port= process.env.PORT ||4000;
 app.use(express.json());
 
 app.use("/api/contacts", require("./routes/contactRoutes"))
+app.use(errorHandler);
 
 app.get("/haii",(req,res)=>{
     res.status(200).send({message:"haii"})
